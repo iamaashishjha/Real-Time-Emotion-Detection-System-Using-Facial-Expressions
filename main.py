743e315fd8611a0a01d3e02e4f5a7a4b0db91ac1
iamaashishjha <mail.aashishjha@gmail.com>
2023-01-27 22:57:18 +0545

    Fixes

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-import cv2
+import cv2, os, argparse
 import numpy as np
-import argparse
 import matplotlib.pyplot as plt
-import os
 import tkinter as tk
+
 from tensorflow import keras
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential, load_model, model_from_json
 from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img,img_to_array
+
 from tkinter import *
 from tkinter import messagebox, filedialog, ttk
 from PIL import Image, ImageTk
 from datetime import datetime
 
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "0"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2" #LOG_LEVEL == 2 FOR WARNING ONLY
+
+DATA_PATH = 'data/test'
+CATEGORIES = os.listdir(DATA_PATH)
+LABELS = [i for i in range(len(CATEGORIES))]
+LABELS_DICTIONARY = dict(zip(CATEGORIES, LABELS))
+
+print(CATEGORIES)
+print(LABELS)
+print(LABELS_DICTIONARY)
+
 
 # Creating object of tk class
 root = tk.Tk()
@@ -54,11 +65,6 @@ style.configure(
 destPath = StringVar()
 imagePath = StringVar()
 
-# command line argument
-ap = argparse.ArgumentParser()
-ap.add_argument("--mode", help="train/display")
-mode = ap.parse_args().mode
-
 # Define data generators
 train_dir = "data/train"
 val_dir = "data/test"
@@ -69,7 +75,7 @@ batch_size = 64
 num_epoch = 50
 
 train_datagen = ImageDataGenerator(rescale=1.0 / 255)
-val_datagen = ImageDataGenerator(rescale=1.0 / 255)
+val_datagen = train_datagen
 
 train_generator = train_datagen.flow_from_directory(
     train_dir,
@@ -105,16 +111,12 @@ model.add(Dense(1024, activation="relu"))
 model.add(Dropout(0.5))
 model.add(Dense(7, activation="softmax"))
 
-# Defining CreateWidgets() function to create necessary tkinter widgets
-def createWidgets():
+# Defining DisplayFeed() function to create necessary tkinter widgets
+def DisplayFeed():
     root.cameraLabel = ttk.Label(
-        root, text="Camera Feed", background="#F0F8FF", padding=10, wraplength = int(width)
+        root, text="Camera Feed", background="#F0F8FF", wraplength = int(width)
     )
     root.cameraLabel.grid(row=4, column=4, sticky="nsew")
-    
-    # root.cameraLabel.config(
-    #     text="Camera Switched Off", font=("Roboto Black", 50), padding=10, background="#F0F8FF", justify="center", wraplength = int(width)
-    # )
     
     root.cameraLabel.place(relx=0.5, rely=0.5, anchor="center")
     
@@ -130,59 +132,88 @@ def createWidgets():
 def ShowEmotionFeed():
     # Capturing frame by frame
     model.load_weights("model.h5")
-
     # prevents openCL usage and unnecessary logging messages
     cv2.ocl.setUseOpenCL(False)
+    # Creating object of class VideoCapture with webcam index
+    root.cap = cv2.VideoCapture(0)
 
-    # dictionary which assigns each label an emotion (alphabetical order)
-    emotion_dict = {
-        0: "Angry",
-        1: "Disgusted",
-        2: "Fearful",
-        3: "Happy",
-        4: "Neutral",
-        5: "Sad",
-        6: "Surprised",
-    }
-
-    ret, frame = root.cap.read()
+    # Setting width and height
+    width, height = 640, 480
+    root.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    root.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
     # Load the Haar cascade classifier
-    facecasc = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-    if ret:
+    face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+
+    res, frame = root.cap.read()
+    if res:
         # Flipping the frame vertically
         frame = cv2.flip(frame, 1)
         # Displaying date and time on the feed
-        cv2.putText(
-            frame,
-            datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
-            (20, 30),
-            cv2.FONT_HERSHEY_DUPLEX,
-            0.5,
-            (0, 255, 255),
-        )
-        # Changing the frame color from BGR to RGB
-        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = facecasc.detectMultiScale(cv2image, scaleFactor=1.3, minNeighbors=5)
+        height, width , channel = frame.shape
+        sub_img = frame[0:int(height/6),0:int(width)]
 
+        black_rect = np.ones(sub_img.shape, dtype=np.uint8)*0
+        res = cv2.addWeighted(sub_img, 0.77, black_rect,0.23, 0)
+        FONT = cv2.FONT_HERSHEY_SIMPLEX
+        FONT_SCALE = 1
+        FONT_THICKNESS = 2
+        LABEL_COLOR = (0, 0, 0)
+        LABEL_COLOR = (0, 0, 0)
+        LABEL_TITLE = "Real Time Emotion Detection"
+        LABEL_DIMENSION = cv2.getTextSize(LABEL_TITLE,FONT,FONT_SCALE,FONT_THICKNESS)[0]
+        textX = int((res.shape[1] - LABEL_DIMENSION[0]) / 2)
+        textY = int((res.shape[0] + LABEL_DIMENSION[1]) / 2)
+        # cv2.putText(frame, LABEL_TITLE, (textX,textY), FONT, FONT_SCALE, LABEL_COLOR, FONT_THICKNESS)
+        text_width, _ = cv2.getTextSize(LABEL_TITLE, FONT, FONT_SCALE, FONT_THICKNESS)[0]
+        rect_x, rect_y = textX - 5, textY - 25
+        rect_width, rect_height = text_width + 10, 30
+        cv2.rectangle(frame, (rect_x, rect_y), (rect_x + rect_width, rect_y + rect_height), LABEL_COLOR, -1)
+        cv2.putText(frame, LABEL_TITLE, (textX,textY), FONT, FONT_SCALE, (255,255,255), FONT_THICKNESS)
+        gray_image= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_haar_cascade.detectMultiScale(gray_image )
+        
         # Draw a rectangle around the faces
         for (x, y, w, h) in faces:
+            # cv2.rectangle(frame, pt1 = (x,y),pt2 = (x+w, y+h), color = (255,0,0),thickness =  2)
             cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
-            roi_gray = cv2image[y : y + h, x : x + w]
-            cropped_img = np.expand_dims(
-                np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0
-            )
-            prediction = model.predict(cropped_img)
-            maxindex = int(np.argmax(prediction))
+            roi_gray = gray_image[y-5:y+h+5,x-5:x+w+5]
+            roi_gray=cv2.resize(roi_gray,(48,48))
+            image_pixels = img_to_array(roi_gray)
+            image_pixels = np.expand_dims(image_pixels, axis = 0)
+            image_pixels /= 255
+            predictions = model.predict(image_pixels)
+            max_index = np.argmax(predictions[0])
+            emotion_prediction = emotion_detection[max_index]
+            emotion_prediction_confidence = str(np.round(np.max(predictions[0])*100,1))+ "%"
+            LABEL_EMOTION = 'Emotion: {}'.format(emotion_prediction)
+            # LABEL_CONFIDENCE = 'Confidence: {}'.format(emotion_prediction_confidence)
+            LABEL_CONFIDENCE = emotion_prediction_confidence
+            
+            violation_text_dimension = cv2.getTextSize(LABEL_CONFIDENCE,FONT,FONT_SCALE,FONT_THICKNESS)[0]
+            # LABEL_X_AXIS = int(res.shape[1]- violation_text_dimension[0])
+            # LABEL_Y_AXIS = textY+22+5
+            LABEL_X_AXIS = x+20
+            LABEL_Y_AXIS = y-60
+            
             cv2.putText(
                 frame,
-                emotion_dict[maxindex],
-                (x + 20, y - 60),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                (255, 255, 255),
-                2,
-                cv2.LINE_AA,
+                LABEL_EMOTION,
+                (0,LABEL_Y_AXIS),
+                FONT,
+                FONT_SCALE,
+                LABEL_COLOR,
+                FONT_THICKNESS,
+            )
+            cv2.putText(
+                frame,
+                LABEL_CONFIDENCE,
+                (LABEL_X_AXIS,LABEL_Y_AXIS),
+                FONT,
+                FONT_SCALE,
+                LABEL_COLOR,
+                FONT_THICKNESS,
             )
         # Display the resulting frame
         videoImg = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
@@ -218,7 +249,30 @@ def StopCam():
     
     root.cameraLabel.place(relx=0.5, rely=0.5, anchor="center")
 
-# Defining StartCam() to start WEBCAM Preview
+# # Defining StartEmotionCam() to start WEBCAM Preview
+# def StartEmotionCam():
+#     # Stop Previous Camera
+#     StopCam()
+
+#     # Creating object of class VideoCapture with webcam index
+#     root.cap = cv2.VideoCapture(0)
+#     # Setting width and height
+#     root.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+#     root.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+#     # Configuring the CAMBTN to display accordingly
+#     root.CAMBTN = ttk.Button(
+#         root, text="Stop Camera", command=StopCam, style="my.TButton"
+#     )
+#     root.CAMBTN.grid(row=5, column=4, pady=10, padx=10, sticky="nsew")
+
+#     # Removing text message from the camera label
+#     root.cameraLabel.config(text="")
+
+#     # Calling the ShowFeed() Function
+#     ShowEmotionFeed()
+    
+    
 # Defining StartEmotionCam() to start WEBCAM Preview
 def StartEmotionCam():
     # Stop Previous Camera
@@ -242,14 +296,30 @@ def StartEmotionCam():
     # Calling the ShowFeed() Function
     ShowEmotionFeed()
     
+    
 def on_mouse(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         # Release the webcam and close all windows when the left mouse button is clicked
         root.cap.release()
         cv2.destroyAllWindows()
 
+
+def trainModel():
+    opt = keras.optimizers.Adam(learning_rate=0.0001)
+    model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+    model_info = model.fit(
+        train_generator,
+        steps_per_epoch=num_train // batch_size,
+        epochs=num_epoch,
+        validation_data=validation_generator,
+        validation_steps=num_val // batch_size,
+    )
+    plot_model_history(model_info)
+    model.save_weights("model.h5")
+    
+    
 if __name__ == "__main__":
-    createWidgets()
+    # DisplayFeed()
     root.mainloop()
     
     
